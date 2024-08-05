447ad3bc000471238edf0b420d10aaabc93c2b6d
AJ <105689704+ajdev05@users.noreply.github.com>
2024-08-05 19:15:38 -0400

    Update app.py

--- app.py
+++ app.py
@@ -89,5 +89,5 @@ def create_app():
 if __name__ == "__main__":
     app = create_app()
     start_db.initialize_database()
-    app.run(debug=True, host="0.0.0.0")
+    app.run(debug=True, host="0.0.0.0", port=5000)
 
